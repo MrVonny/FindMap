53d3d6cef38c9b35d37dd2dfd52361072b6defec
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-09-04 01:34:55 +0300

    more maps

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { ComposableMap, Geographies, Geography } from "react-simple-maps"
 import './App.css';
 
 const geoUrl =
-    "D:\\repo\\FindMap\\src\\GeoJSON\\map.topojson"
+    "https://raw.githubusercontent.com/MrVonny/FindMap/master/src/GeoJSON/map.topojson"
 
 
 function App() {
